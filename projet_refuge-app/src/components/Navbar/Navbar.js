a1cbe719c6bbaa73ef1b524b644305a8bd8639a2
alvarezadrien <adrienalvarez15@gmail.com>
2024-11-05 16:56:09 +0100

    Création de sous dossier + de fichier css, changement de code et rajout de code css et delete de css

---
+++ projet_refuge-app/src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './index.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-logo">Mon Site</h1>
+      <ul className="navbar-links">
+        <li><a href="/">Accueil</a></li>
+        <li><a href="/about">À propos</a></li>
+        <li><a href="/services">Services</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
