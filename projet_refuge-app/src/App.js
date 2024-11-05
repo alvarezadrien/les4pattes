a973acb731626b61cb86b368cf33d98883c4e339
alvarezadrien <adrienalvarez15@gmail.com>
2024-11-05 17:41:06 +0100

    Rajout import

--- projet_refuge-app/src/App.js
+++ projet_refuge-app/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home'
 import Footer from './components/Footer'
+import './index.css';
+
 
 
 function App() {
