2cedd0aaf6aa693515bacb285b4699099e1948ee
alvarezadrien <adrienalvarez15@gmail.com>
2024-11-06 22:12:31 +0100

    Rajout route hommePage, rajout code css

--- projet_refuge-app/src/components/Home/HomePage.js
+++ projet_refuge-app/src/components/Home/HomePage.js
@@ -1,9 +1,14 @@
 import React from "react";
+import '../Home/HomePage.css';
+import Navbar from "../Navbar/Navbar";
 
 const HomePage = () => {
     return (
+        <div>
+            <Navbar />
         <div className="container_home">
             <h1>Hello world!</h1>
+        </div>
         </div>
     );
 };
