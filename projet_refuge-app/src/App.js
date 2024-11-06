7e5b15c05f5d7d2b625630f9c3aa77cf2bf3b39c
alvarezadrien <adrienalvarez15@gmail.com>
2024-11-06 19:05:59 +0100

    Rajout import navbar et footer, rajout de rooter etc.. code css pour nav_ul, déplacement des fichier dans sous dossier

--- projet_refuge-app/src/App.js
+++ projet_refuge-app/src/App.js
@@ -1,33 +1,24 @@
 import React from "react";
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar/Navbar'
-import Home from './components/Home'
-import Footer from './components/Footer'
-import './index.css';
+// import Navbar from './components/Navbar/Navbar'
+// import Home from './components/Home'
+// import Footer from './components/Footer'
+// import './index.css';
 
 
 
 function App() {
   return (
-    <nav className="navbar">
-      <div className="logo-nav">
-        <a href="home.html">
-          <img
-            src="../public/img/logo cafet.webp"
-            alt=""
-            width="100px"
-            height="100px"
-          /></a>
-
-        <ul className="nav_ul">
-          <li><a href="#">Refuge</a></li>
-          <li><a href="#">Animaux</a></li>
-          <li><a href="#">Adoption</a></li>
-          <li><a href="#">Membres</a></li>
-
-        </ul>
-      </div>
-    </nav>
+    <router>
+        <div>
+          <routes>
+              <route path="/Navabar" element={<Navbar />}/>
+          </routes>
+        </div>
+    </router>
   );
 };
 
--- projet_refuge-app/src/components/Navbar/Navbar.js
+++ projet_refuge-app/src/components/Navbar/Navbar.js
@@ -1,9 +1,29 @@
 import React from "react";
-import '../Navbar/Navbar.css'
+import './Navbar.css'
 
 function Navbar() {
   return (
     <nav className="navbar">
+      <div className="logo-nav">
+        <a href="home.html">
+          <img
+            src="../public/img/logo cafet.webp"
+            alt=""
+            width="100px"
+            height="100px"
+          /></a>
+
+        <ul className="nav_ul">
+          <li><a href="#">Refuge</a></li>
+          <li><a href="#">Animaux</a></li>
+          <li><a href="#">Adoption</a></li>
+          <li><a href="#">Membres</a></li>
+          <li><a href="#">Contact</a></li>
+
+        </ul>
+      </div>
     </nav>
   );
 };
+
+export default Navbar;
