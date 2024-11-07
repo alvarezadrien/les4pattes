74e6170740f37b268aa29391728cf7e814de45f2
alvarezadrien <adrienalvarez15@gmail.com>
2024-11-07 13:08:20 +0100

    rajout commande yarn et changement code app.js

--- projet_refuge-app/src/App.js
+++ projet_refuge-app/src/App.js
@@ -7,8 +7,10 @@ import './index.css';
 const App = () => {
   return (
     <Router>
+      <Navbar /> {/* Assure-toi que ton Navbar est présent sur toutes les pages */}
       <div>
         <Routes>
+          <Route path="/" element={<HomePage />} />  {/* Page d'accueil */}
           <Route path="/HomePage" element={<HomePage />} />
           <Route path="/refuge" element={<div>Refuge Page</div>} />
           <Route path="/animaux" element={<div>Animaux Page</div>} />
