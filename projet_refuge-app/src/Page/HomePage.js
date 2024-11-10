b0de3dcfff9767e3597dfc225e58f9d1aea420d9
alvarezadrien <adrienalvarez15@gmail.com>
2024-11-10 17:25:58 +0100

    Rajout icon pattes, fini div container, rajout de size et img

--- projet_refuge-app/src/Page/HomePage.js
+++ projet_refuge-app/src/Page/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
     return (
         <div className='container_img'>
             <Navbar />
-            <h1 className='h1_titre'>Les 4 pattes</h1>
+            <h1 className='h1_titre'>Les 4 pattes <img src="/img/carte_pointer.png" alt="" width={40} height={40}/></h1>
 
             <p className='paragraphe_container'>Notre association pour les animaux <br />
                 perdus et abandonnés, depuis <br />
@@ -16,17 +16,17 @@ const HomePage = () => {
 
             <div className='circle_img0'>
                 <div className='circle_img1'>
-                    <img src="/img/img_chienchat_container.jpg" alt="" width={280} height={305} />
+                    <img src="/img/img_chienchat_container.jpg" alt="" width={280} height={290} />
 
                 </div>
                 <div className='circle_img2'>
                     <img src="/img/chien.jpg" alt="" width={235} height={210}/>
                 </div>
                 <div className='circle_img3'>
-                    <img src="/img/images.jpeg" alt="" width={210} height={210}/>
+                    <img src="/img/images.jpeg" alt="" width={200} height={200}/>
                 </div>
             </div>
-            <button type='button' className='button_propos'>à propos</button>
+            <button type='button' className='button_propos'>À propos</button>
         </div>
 
     )
