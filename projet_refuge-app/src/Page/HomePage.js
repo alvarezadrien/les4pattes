69c824b72d3badc5f3d5b94065322e5a6d3e7c2c
alvarezadrien <adrienalvarez15@gmail.com>
2024-11-10 11:57:45 +0100

    Rajout img dans projet, rajout code css + container rajout de paragraphe et h1 et img

--- projet_refuge-app/src/Page/HomePage.js
+++ projet_refuge-app/src/Page/HomePage.js
@@ -4,9 +4,20 @@ import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
     return (
-        <div>
+        <div className='container_img'>
             <Navbar />
-            HomePage
+            <h1>Les 4 pattes</h1>
+
+            <p className='paragraphe_container'>Notre association pour les animaux <br />
+                perdus et abandonnés, depuis <br />
+                plus de 100 ans en Belgique
+            </p>
+            <br />
+
+            <div className='circle_img'>
+                <img src="/img/img_chienchat_container.jpg" alt="" width={280} height={325} />
+
+            </div>
         </div>
 
     )
