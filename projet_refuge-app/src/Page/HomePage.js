d24f4e4df8ebecefa80a5ee26d157697bd3ed4f5
alvarezadrien <adrienalvarez15@gmail.com>
2024-11-10 17:01:33 +0100

    rajout img chien, code css pour HomePage; rajout de div img circle

--- projet_refuge-app/src/Page/HomePage.js
+++ projet_refuge-app/src/Page/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
     return (
         <div className='container_img'>
             <Navbar />
-            <h1>Les 4 pattes</h1>
+            <h1 className='h1_titre'>Les 4 pattes</h1>
 
             <p className='paragraphe_container'>Notre association pour les animaux <br />
                 perdus et abandonnés, depuis <br />
@@ -14,10 +14,19 @@ const HomePage = () => {
             </p>
             <br />
 
-            <div className='circle_img'>
-                <img src="/img/img_chienchat_container.jpg" alt="" width={280} height={325} />
+            <div className='circle_img0'>
+                <div className='circle_img1'>
+                    <img src="/img/img_chienchat_container.jpg" alt="" width={280} height={305} />
 
+                </div>
+                <div className='circle_img2'>
+                    <img src="/img/chien.jpg" alt="" width={235} height={210}/>
+                </div>
+                <div className='circle_img3'>
+                    <img src="/img/images.jpeg" alt="" width={210} height={210}/>
+                </div>
             </div>
+            <button type='button' className='button_propos'>à propos</button>
         </div>
 
     )
