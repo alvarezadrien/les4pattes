7f169d6d2f8045c6cfbd0ff8a6531fef19e680af
alvarezadrien <adrienalvarez15@gmail.com>
2024-11-13 01:03:56 +0100

    New img profil,  changement code css header.  rajout code JSX img coeur et login header

--- projet_refuge-app/src/components/Navbar/Navbar.js
+++ projet_refuge-app/src/components/Navbar/Navbar.js
@@ -15,13 +15,21 @@ const Navbar = () => {
           /></a>
 
         <ul className="nav_ul">
-          <li><a href="#">Refuge</a></li>
           <li><a href="#">Animaux</a></li>
           <li><a href="#">Adoption</a></li>
           <li><a href="#">Membres</a></li>
           <li><a href="#">Contact</a></li>
-
         </ul>
+
+        <div className="coeur_don">
+          <img src="/img/coeurs.png" alt="" width={45} height={45}/>
+          {/* <h2 className="h_don">Faire un don</h2> */}
+        </div>
+
+        <div className="icon_connect">
+          <img src="/img/profil.png" alt="" width={40} height={40}/>
+        </div>
+
       </div>
     </nav>
   );
