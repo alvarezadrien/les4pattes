c992665cd74061c57235049264beddd088504be1
alvarezadrien <adrienalvarez15@gmail.com>
2024-11-13 18:51:57 +0100

    Renome img rajout code jsx rajout de div className et img

--- projet_refuge-app/src/Page/Apropos.js
+++ projet_refuge-app/src/Page/Apropos.js
@@ -7,8 +7,33 @@ const Apropos = () => {
         <div>
             <div className="container_img_propos">
                 <div className="img0">
-                    <img src="/img/img_etablissement.jpg" alt="" width={550} height={250}/>
+                    <img src="/img/img_etablissement.jpg" alt="" />
                 </div>
+                
+                {/* Côté gauche */}
+
+                <div className="img_1">
+                    <img src="/img/téléchargement (1).jpeg" alt="" />
+                </div>
+                <div className="img_2">
+                    <img src="/img/téléchargement (2).jpeg" alt="" />
+                </div>
+
+                    {/* Côté droit */}
+
+                <div className="img_3">
+                    <img src="/img/téléchargement(3).jpeg" alt="" />
+                </div>
+                <div className="img_4">
+                    <img src="/img/téléchargement (4).jpeg" alt="" />
+                </div>
+            </div>
+
+            {/* Div pattes propos */}
+            <div className="pattes_propos">
+                <h2>Les 4 pattes</h2>
+
+                <img src="/img/img_aide_soins.jpg" alt="" width={400} height={360}/>
             </div>
         </div>
     );
