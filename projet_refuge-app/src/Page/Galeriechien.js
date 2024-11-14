30c93d299bc4214f9a09a722ff6539578f751c7d
alvarezadrien <adrienalvarez15@gmail.com>
2024-11-14 19:33:55 +0100

    renome img,   rajout code css page galeriechien;   rajout className div img et import css

--- projet_refuge-app/src/Page/Galeriechien.js
+++ projet_refuge-app/src/Page/Galeriechien.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import '../Galeriechien.css'
 
 const Galeriechien = () => {
     return (
-        <div>Galeriechien</div>
+        <div className='container_galerie'>
+            <div className='container_img_galerie'>
+                <div className='img_galerie1'>
+                    <img src="/img/chien chat .jpeg" alt="" />
+                </div>
+                <div className='img_galerie2'>
+                    <img src="/img/chat_galerie.jpg" alt="" />
+                </div>
+            </div>
+
+        </div>
     )
 }
 
