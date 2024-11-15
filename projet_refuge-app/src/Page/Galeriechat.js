e71d8aacaa2f9b50f876cc52164fd237d8ef135f
alvarezadrien <adrienalvarez15@gmail.com>
2024-11-15 01:37:50 +0100

    mofif

--- projet_refuge-app/src/Page/Galeriechat.js
+++ projet_refuge-app/src/Page/Galeriechat.js
@@ -5,27 +5,25 @@ import Fichegalerie from './Fiche_galeriechat';
 import Pagination from './Pagination';
 
 const Galeriechat = () => {
-    const totalPages = 5; // Définir le nombre total de pages ici
+    const totalPages = 5; // Nombre total de pages
+    const images = [
+        { src: "/img/chien chat .jpeg", alt: "Chien et chat ensemble" },
+        { src: "/img/chat_galerie.jpg", alt: "Chat dans la galerie" },
+        { src: "/img/img_chien_galerie.jpg", alt: "Chien dans la galerie" },
+        { src: "/img/img_chat_galerie.jpg", alt: "Chat dans la galerie" }
+    ];
 
     return (
         <div className='container_galerie'>
             <div className='container_img_galerie'>
-
                 <h1 className='h1_proteges'>Nos protégés</h1>
 
                 <div className='divimg_principal'>
-                    <div className='img_galerie1'>
-                        <img src="/img/chien chat .jpeg" alt="" />
-                    </div>
-                    <div className='img_galerie2'>
-                        <img src="/img/chat_galerie.jpg" alt="" />
-                    </div>
-                    <div className='img_galerie3'>
-                        <img src="/img/img_chien_galerie.jpg" alt="" />
-                    </div>
-                    <div className='img_galerie4'>
-                        <img src="/img/img_chat_galerie.jpg" alt="" />
-                    </div>
+                    {images.map((image, index) => (
+                        <div key={index} className={`img_galerie${index + 1}`}>
+                            <img src={image.src} alt={image.alt} />
+                        </div>
+                    ))}
                 </div>
             </div>
 
