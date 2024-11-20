90112b822ae1873fe85f33c378141af361988323
alvarezadrien <adrienalvarez15@gmail.com>
2024-11-20 17:01:32 +0100

    rajout css code et dic et class

--- projet_refuge-app/src/components/Footer/Footer.js
+++ projet_refuge-app/src/components/Footer/Footer.js
@@ -5,28 +5,31 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="logo_footer">
-                <img src="/img/logo_refuge.png" alt="" width={180} height={180} />
-            </div>
-            <div className="icon_email_h2">
-                <h2 className="icon_email"><img src="/img/email.png" alt="" />Contactez-nous</h2>
-                <ul className="liste_contact_footer">
-                    <li><a href="tel:+32492764208">+32492764208</a></li>
-                    <li><a href="mailto:adrienalvarez15@gmail.com">adrienalvarez15@gmail.com</a></li>
-                    <li><address>Rue des papillons 245, 1070 Bruxelles</address></li>
-                    <li>Numéro d'agrément: Db 452 21 302</li>
-                </ul>
-            </div>
+            <div className="footer_haut">
+                <div className="logo_footer">
+                    <img src="/img/logo_refuge.png" alt="" width={180} height={180} />
+                </div>
+                <div className="icon_email_h2">
+                    <h2 className="icon_email"><img src="/img/email.png" alt="" />Contactez-nous</h2>
+                    <ul className="liste_contact_footer">
+                        <li><a href="tel:+32492764208">+32492764208</a></li>
+                        <li><a href="mailto:adrienalvarez15@gmail.com">adrienalvarez15@gmail.com</a></li>
+                        <li><address>Rue des papillons 245, 1070 Bruxelles</address></li>
+                        <li>Numéro d'agrément: Db 452 21 302</li>
+                    </ul>
+                </div>
 
 
-            <div className="icon_horaire_h2">
-                <h2><img src="/img/horloge.png" alt="" />Horaires</h2>
-                <ul className="liste_horaire_footer">
-                    <li>Lundi-Samedi: 10h à 16h sur</li>
-                    <li>rendez-vous au <a href="tel:0492764208">04/527.10.50</a></li>
-                    <li>Fermé les dimanches et les jours fériés.</li>
-                </ul>
+                <div className="icon_horaire_h2">
+                    <h2><img src="/img/horloge.png" alt="" />Horaires</h2>
+                    <ul className="liste_horaire_footer">
+                        <li>Lundi-Samedi: 10h à 16h sur</li>
+                        <li>rendez-vous au <a href="tel:0492764208">04/527.10.50</a></li>
+                        <li>Fermé les dimanches et les jours fériés.</li>
+                    </ul>
+                </div>
             </div>
+
 
             <div className="footer_bas">
                 <div className="reseaux_sociaux">
