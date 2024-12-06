c50e73fb3313738a1ff19967f753f1f2aa928b3e
alvarezadrien <adrienalvarez15@gmail.com>
2024-12-06 13:08:47 +0100

    changement code js

--- projet_refuge-app/src/Page/Mon_compte.js
+++ projet_refuge-app/src/Page/Mon_compte.js
@@ -4,34 +4,52 @@ import '../Mon_compte.css';
 const MonCompte = () => {
     const [isFirstClick, setIsFirstClick] = useState(true);
 
+    const [image1Src, setImage1Src] = useState("/img/Magazine1.jpg");
+    const [image2Src, setImage2Src] = useState("/img/Magazine2.jpg");
+
+    const [image1Style, setImage1Style] = useState({
+        width: '150px',
+        transform: 'scale(1.2)',
+        order: 1
+    });
+
+    const [image2Style, setImage2Style] = useState({
+        width: '100px',
+        transform: 'scale(0.8)',
+        order: 2
+    });
+
     const changeImage = () => {
-        const magazine1 = document.getElementById('magazine1');
-        const magazine2 = document.getElementById('magazine2');
-
         if (isFirstClick) {
-            const tempSrc = magazine1.src;
-            magazine1.src = magazine2.src;
-            magazine2.src = tempSrc;
+            setImage1Src("/img/Magazine2.jpg");
+            setImage2Src("/img/Magazine1.jpg");
 
-            magazine2.style.width = '150px';
-            magazine1.style.width = '100px';
-            magazine1.style.transform = 'scale(0.8)';
-            magazine2.style.transform = 'scale(1.2)';
+            setImage1Style({
+                width: '100px',
+                transform: 'scale(0.8)',
+                order: 2
+            });
 
-            magazine1.style.order = 2;
-            magazine2.style.order = 1;
+            setImage2Style({
+                width: '150px',
+                transform: 'scale(1.2)',
+                order: 1
+            });
         } else {
-            const tempSrc = magazine1.src;
-            magazine1.src = magazine2.src;
-            magazine2.src = tempSrc;
+            setImage1Src("/img/Magazine1.jpg");
+            setImage2Src("/img/Magazine2.jpg");
 
-            magazine1.style.width = '150px';
-            magazine2.style.width = '100px';
-            magazine1.style.transform = 'scale(1.2)';
-            magazine2.style.transform = 'scale(0.8)';
+            setImage1Style({
+                width: '150px',
+                transform: 'scale(1.2)',
+                order: 1
+            });
 
-            magazine1.style.order = 1;
-            magazine2.style.order = 2;
+            setImage2Style({
+                width: '100px',
+                transform: 'scale(0.8)',
+                order: 2
+            });
         }
 
         setIsFirstClick(!isFirstClick);
@@ -65,15 +83,15 @@ const MonCompte = () => {
                 <div className="container_img_magazine">
                     <img
                         className="magazine1"
-                        src="/img/Magazine1.jpg"
-                        id="magazine1"
+                        src={image1Src}
+                        style={image1Style}
                         onClick={changeImage}
                         alt="Magazine 1"
                     />
                     <img
                         className="magazine2"
-                        src="/img/Magazine2.jpg"
-                        id="magazine2"
+                        src={image2Src}
+                        style={image2Style}
                         onClick={changeImage}
                         alt="Magazine 2"
                     />
