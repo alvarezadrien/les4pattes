105c110df70c621af4444987a4236e4e6e92bf8e
alvarezadrien <adrienalvarez15@gmail.com>
2024-12-25 07:44:23 +0100

    Changement de police fonts pour tout les h titre et span et paragraphe

--- projet_refuge-app/src/App.js
+++ projet_refuge-app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
