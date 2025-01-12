b03ecd6b4a88a284a963e990d3d54d500b9a4245
alvarezadrien <adrienalvarez15@gmail.com>
2025-01-12 19:50:22 +0100

    changement d'info fiche chat et de img et rajout

--- projet_refuge-app/src/Page/Fiche_galeriechat.js
+++ projet_refuge-app/src/Page/Fiche_galeriechat.js
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 
 const Fichegalerie = () => {
     const cat = [
-        { name: "Diablo", age: "2 ans 1/2", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chat_galeriefiche.jpg" },
-        { name: "Bella", age: "1 an", race: "Lorem", Sexe: "Femelle stérilisé", imgSrc: "/img/chat_galeriefiche.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chat_galeriefiche.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chat_galeriefiche.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chat_galeriefiche.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chat_galeriefiche.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chat_galeriefiche.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chat_galeriefiche.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chat_galeriefiche.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chat_galeriefiche.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chat_galeriefiche.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chat_galeriefiche.jpg" },
+        { name: "Oreo", age: "1 An 1/2", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechat/oreo_1.2.jpg" },
+        { name: "Bella", age: "1 an", race: "Européen", Sexe: "Femelle stérilisé", imgSrc: "/img/img_galeriechat/chat_roux_1.1.jpg" },
+        { name: "Coco", age: "1 An 1/2", race: "Siamois", Sexe: "Femelle stérilisé", imgSrc: "/img/img_galeriechat/chat_siamois_1.2.jpg" },
+        { name: "Rocky", age: "5 Ans", race: "Européen", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechat/chat_gris_1.2.jpg" },
+        { name: "Buddy", age: "1 An", race: "Bengal", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechat/chat_bengal_1.3.jpg" },
+        { name: "Bogart", age: "3 Ans", race: "Européen", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechat/chat_blancnoir1.webp" },
+        { name: "Casper", age: "2 Ans", race: "Européen", Sexe: "Femelle stérilisé", imgSrc: "/img/img_galeriechat/chat_blanc_1.3.jpg" },
+        { name: "Nala", age: "8 Mois", race: "ecaille de tortue", Sexe: "Femelle stérilisé", imgSrc: "/img/img_galeriechat/nala_1.1.jpg" },
+        { name: "Luna", age: "7 Mois", race: "Européen", Sexe: "Femelle stérilisé", imgSrc: "/img/img_galeriechat/lune_1.2.jpg" },
+        { name: "Gizmo", age: "7 Ans 1/2", race: "Européen", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechat/chat_pexels_1.3.jpg" },
+        { name: "Misty", age: "8 Ans", race: "Européen", Sexe: "Femelle stérilisé", imgSrc: "/img/img_galeriechat/chat_grisblanc_1.3.png" },
+        { name: "Leo", age: "3 Ans", race: "Européen", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechat/chat_grisligne_1.3.png" },
     ];
 
     return (
