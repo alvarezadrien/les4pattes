8aaa2355faf4f04ad483faedadbccce8e059571c
alvarezadrien <adrienalvarez15@gmail.com>
2025-01-12 21:35:51 +0100

    rajout new img et change information card

--- projet_refuge-app/src/Page/Fiche_galeriechien.js
+++ projet_refuge-app/src/Page/Fiche_galeriechien.js
@@ -5,18 +5,18 @@ import { Link } from "react-router-dom";
 const Fiche_galeriechien = () => {
     // Appel variable chiens
     const dogs = [
-        { name: "Diablo", age: "2 ans 1/2", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chien_fiche1.jpg" },
-        { name: "Bella", age: "1 an", race: "Lorem", Sexe: "Femelle stérilisé", imgSrc: "/img/chien_fiche1.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chien_fiche1.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chien_fiche1.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chien_fiche1.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chien_fiche1.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chien_fiche1.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chien_fiche1.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chien_fiche1.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chien_fiche1.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chien_fiche1.jpg" },
-        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/chien_fiche1.jpg" },
+        { name: "Diablo", age: "2 ans 1/2", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechien/chien_malinois_2.1.jpg" },
+        { name: "Bella", age: "1 an", race: "Lorem", Sexe: "Femelle stérilisé", imgSrc: "/img/img_galeriechien/chien_brun_1.2.jpg" },
+        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechien/chien_noire_4.1.jpg" },
+        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechien/chien_petit_1.3.jpeg" },
+        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechien/chien_assis_1.1.jpeg" },
+        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechien/chien_terrier_1.2.jpg" },
+        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechien/chien_berger_1.3.jpg" },
+        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechien/chien_mali_1.1.jpg" },
+        { name: "Max", age: "3 Ans", race: "Lorem", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechien/chien_iron_1.1.jpg" },
+        { name: "Otto", age: "2 Ans", race: "American Stafforshire Terrier", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechien/chien_staff_1.2.jpg" },
+        { name: "Mila", age: "1 An 1/2", race: "Jack Russell", Sexe: "Femelle stérilisé", imgSrc: "/img/img_galeriechien/chien_russell_1.1.jpg" },
+        { name: "Achille", age: "4 Ans", race: "croisé, Dogue argentin nom", Sexe: "Mâle stérilisé", imgSrc: "/img/img_galeriechien/chien_dogue_1.3.jpg" },
 
     ];
 
