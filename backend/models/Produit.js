eda4bca2312220b5c7d07399f1ce21c750342b3c
alvarezadrien <adrienalvarez15@gmail.com>
2025-08-06 23:41:15 +0200

    faire que plusieur catégories sur le même produit soit possible via un tableau

--- backend/models/Produit.js
+++ backend/models/Produit.js
@@ -1,28 +1,28 @@
 const mongoose = require("mongoose");
 
 const produitSchema = new mongoose.Schema({
-    nom: { type: String, required: true },
-    description: String,
-    prix: String,
-    image: String,
-    espece: String,
-    categorie: {
-        type: String,
-        enum: [
-            "chien",
-            "chat",
-            "humain",
-            "jouet",
-            "croquettes",
-            "accessoires",
-            "friandises",
-            "goodies",
-            "vêtements"
-        ],
-        required: true
-    },
-    stock: { type: Number, default: 0 },
-    poids: String
+  nom: { type: String, required: true },
+  description: String,
+  prix: String,
+  image: String,
+  espece: String,
+  categorie: {
+    type: [String], 
+    enum: [
+      "chien",
+      "chat",
+      "humain",
+      "jouet",
+      "croquettes",
+      "accessoires",
+      "friandises",
+      "goodies",
+      "vêtements"
+    ],
+    required: true
+  },
+  stock: { type: Number, default: 0 },
+  poids: String
 });
 
 module.exports = mongoose.model("Produit", produitSchema);
